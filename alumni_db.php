12638bceb6688591576aea460a3c4d0d87c8586e
Salvador P <nyakebei.ipn@gmail.com>
2019-06-19 02:44:45 -0500

    Added php file that contains info for alumni cards

---
+++ alumni_db.php
@@ -0,0 +1,26 @@
+<?php
+$current_name = array();
+$current_from = array();
+$current_proyect = array();
+$current_proyect_id = array();
+$current_li = array();
+$current_email = array();
+$current_desc = array();
+
+$current_name[] = "Jonathan Adrián Mendieta Antúnez";
+$current_from[] = "Estudiante | CIC IPN";
+$current_proyect[] = "Interpretación de lenguaje no verbal";
+$current_proyect_id[] = "";
+$current_li[] = "http://linkedin.com/in/jonathan-adri%C3%A1n-mendieta-ant%C3%BAnez-09999a116";
+$current_email[] = "adrian.mendieta@deepdaemon.org";
+$current_desc[] = "Ingeniero Mecatrónico de la Universidad La Salle y actual estudiante de maestría en el Centro de Investigación en Computación del Instituto Politécnico Nacional."
+
+$current_name[] = "Adriana Vásquez Ramírez";
+$current_from[] = "Estudiante | ESCOM IPN";
+$current_proyect[] = "Reconocimiento visual de hongos";
+$current_proyect_id[] = "";
+$current_li[] = "";
+$current_email[] = "";
+$current_desc[] = "Soy estudiante de la carrera Ingeniería Sistemas Computacionales, Mis objetivos son aprender nuevas cosas sobre mi carrera para crear proyectos innovadores."
+
+?>
